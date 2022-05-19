53fb2138b1b1c776130641afa3da55432a4cffeb
Diego del Cerro <diego.del-cerro@expleogroup.com>
2022-05-19 12:41:51 +0200

    Agrego otra modificacion del html

---
+++ Untitled-1.py
@@ -0,0 +1,7 @@
+numero_1 = int(input("Dime un nro"))
+numnero_2 = int(input("Dime un nro"))
+numero_3 = int(input("Dime un nro"))
+
+el_maximo = max(numero_1,numero_2,numero_3)
+
+print("El numero mas grande de los 3 es:"+el_maximo)
